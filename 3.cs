abce9d54684c59a5086bd9b48cc9e0a64b6dfa23
pryfmse <nst-dmv@yandex.ru>
2024-10-03 18:02:00 +0700

    Ybxtuj.

---
+++ 3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ПИ;
+
+class Pk
+{
+    public string Model { get; set; }
+    public double ClockSpeed { get; set; }
+    public int RamSize { get; set; }
+    public int HddSize { get; set; }
+
+    public string Info()
+    {
+        return $"Модель: {Model}, Тактовая частота: {ClockSpeed} ГГц, " + 
+               $"ОЗУ: {RamSize} ГБ, Жесткий диск: {HddSize} ГБ, ";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Pk Pk1 = new()
+        {
+            Model = Console.ReadLine(),
+            ClockSpeed = Double.Parse(Console.ReadLine()),
+            RamSize = Int32.Parse(Console.ReadLine()),
+            HddSize = Int32.Parse(Console.ReadLine())
+        };
+
+        Console.WriteLine(Pk1.Info());
+    }
+}
